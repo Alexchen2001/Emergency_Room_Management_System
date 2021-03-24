ab29d1263f1271eea9c99967379b194f48770c57
Alexchen1120 <Alexander.Chen@seattlecolleges.edu>
2021-03-24 14:52:34 -0700

    "Has a problem _ update"

--- src/activities/AlgoProgram.java
+++ src/activities/AlgoProgram.java
@@ -1,4 +1,47 @@
 package activities;
 
+import utility.List;
+import utility.ArrayList;
+import utility.Module;
+
 public class AlgoProgram {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        List<String> list = new ArrayList<String>(6);
+        list.add("Sam");
+        list.add("Cam");
+        list.add("Joe");
+        list.add("Bob");
+        list.add("Abe");
+
+        System.out.println(list);
+        quickSort(list);
+        System.out.println();
+        System.out.println(list);
+
+        List<Person> plist = new ArrayList<Person>();
+        Person mike1 = new Person("Mike JOnes", 42);
+        Person mike2 = new Person("Mike Green", 42);
+        Person mike3 = new Person("Mike Brown", 42);
+        Person sam2 = new Person("Sam Long", 35);
+        Person sam1 = new Person ("Sam Jones", 26);
+        Person minh = new Person("Ming JOnes", 19);
+        plist.add(mike3);
+        plist.add(sam1);
+        plist.add(mike2);
+        plist.add(minh);
+        plist.add(mike1);
+        plist.add(sam2);
+
+        System.out.println(plist);
+        quickSort(plist);
+        System.out.println();
+        System.out.println(plist);
+
+
+
+
+    }
+
 }
--- src/activities/EmergencyRoomProgram.java
+++ src/activities/EmergencyRoomProgram.java
@@ -1,4 +1,20 @@
 package activities;
 
+import utility.List;
+import utility.LinkedList;
+import utility.Module;
+import utility.MyQueue;
+
 public class EmergencyRoomProgram {
+
+
+    public static void main(String[] args) {
+        MyQueue<Person> WaitingLine = new MyQueue<Person>();
+    }
+
+
+
+
+
+
 }
