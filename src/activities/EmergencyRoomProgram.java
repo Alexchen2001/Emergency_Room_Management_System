8a06124d4ef5ce85b9ffaec1245fc83260e8c49b
Alexchen1120 <Alexander.Chen@seattlecolleges.edu>
2021-03-24 17:14:41 -0700

    "update_partial"

--- src/activities/EmergencyRoomProgram.java
+++ src/activities/EmergencyRoomProgram.java
@@ -1,11 +1,22 @@
 package activities;
 
 import utility.List;
-import utility.LinkedList;
+import utility.ArrayList;
 import utility.Module;
 import utility.MyQueue;
 
 public class EmergencyRoomProgram {
+
+    public void organize(ArrayList<Person> list){
+        MyQueue<Person> newLine = 
+        quickSort(list);
+        for (int i = 0; i < list.size(); i++) {
+
+
+        }
+
+        }
+    }
 
 
     public static void main(String[] args) {
